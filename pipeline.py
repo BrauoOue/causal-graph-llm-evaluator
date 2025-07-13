eae5d943566b8eaa68195e2f351cc6142841fb1f
Itonkdong <kostadinoskiviktor@yahoo.com>
2025-07-13 13:57:27 +0200

    add conversion

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,2 @@
-import pandas as pd
-
 if __name__ == '__main__':
-    train = pd.read_json("./dataset-e-Care/train_full.jsonl", lines=True)
-    test = pd.read_json("./dataset-e-Care/dev_full.jsonl", lines=True)
-    full = pd.concat([train, test], axis=0)
-    full.to_json("./dataset-e-Care/full.jsonl", orient='records', lines=True)
-    print(full)
+    print("Hello World")
